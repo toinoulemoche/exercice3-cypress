f9e06fc427e0d85df1b0477cea5818460e3c2c03
toinoulemoche <avaurs28@gmail.com>
2022-06-02 10:26:59 +0200

    compteur pour exercice 3

---
+++ exercice3/src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import {ref} from "vue";
+const compt = 0;
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+     
+    </div>
+  </header>
+
+    <p>{{compt}}</p>
+    <button v-on:click="compt=++">incr</button>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
